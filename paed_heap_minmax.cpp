27f1c1ca599a5f6beb6592a9e825eb02d01c3c71
painkiller778 <painkiller778@gmail.com>
2012-11-29 18:28:26 -0300

    Adicionado scroll e fechar pelo ESC

--- paed_heap_minmax.cpp
+++ paed_heap_minmax.cpp
@@ -12,9 +12,9 @@
 using namespace math;
 using namespace std;
 
-#define SCREEN_W 800
-#define SCREEN_H 640
-#define DEFAULT_RADIUS 16
+#define SCREEN_W 900
+#define SCREEN_H 600
+#define DEFAULT_RADIUS 10
 
 typedef struct Tno
 {
@@ -25,7 +25,18 @@ typedef struct Tno
 
 ALLEGRO_BITMAP* buffer;
 ALLEGRO_DISPLAY* screen;
-ALLEGRO_FONT *font;
+ALLEGRO_FONT* font;
+
+ALLEGRO_EVENT_QUEUE* event_queue = NULL;
+ALLEGRO_TIMER* timer = NULL;
+bool redraw = true;
+
+const float FPS = 60;
+
+int _size = 220;
+int _angle = 5;
+
+Vector2D camera;
 
 int heap_length;
 
@@ -37,7 +48,7 @@ void update_screen()
  
 bool init_double_buffering()
 {
-  buffer = al_create_bitmap(SCREEN_W, SCREEN_H);
+  buffer = al_create_bitmap(3000, SCREEN_H);
   
   if(!buffer)
      return false;
@@ -67,7 +78,7 @@ bool init()
     return -1;
   }
 
-  font = al_load_font("FreeMono.ttf", 24, 0);
+  font = al_load_font("FreeMono.ttf", 15, 0);
 
   if (!font)
   {
@@ -93,17 +104,41 @@ bool init()
     return 1;
   }
 
+  timer = al_create_timer(1.0 / FPS);
+  if(!timer) {
+    fprintf(stderr, "failed to create timer!\n");
+    return -1;
+  }
+
+  event_queue = al_create_event_queue();
+  if(!event_queue) {
+    fprintf(stderr, "failed to create event_queue!\n");
+    al_destroy_display(screen);
+    al_destroy_timer(timer);
+    return -1;
+  }
+
+  al_install_keyboard();
+
+  al_register_event_source(event_queue, al_get_display_event_source(screen));
+
+  al_register_event_source(event_queue, al_get_timer_event_source(timer));
+  
+  al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+  al_start_timer(timer);
+
   return true;
 }
 
 void draw_heap(int * heap)
 {
-  Vector2D start = Vector2D((SCREEN_W - DEFAULT_RADIUS * 2)/2 + DEFAULT_RADIUS, 20); 
-  float angle = 10;
+  Vector2D start = Vector2D((SCREEN_W - DEFAULT_RADIUS * 2)/2 + DEFAULT_RADIUS, 20) + camera; 
+  float angle = _angle;
 
   int mult = 1;
+  int size = _size;
   int i = 1;
-  int size = 200;
   int soma = 0;
 
   queue<Vector2D> toDraw;
@@ -121,8 +156,8 @@ void draw_heap(int * heap)
     Vector2D aux = start + dirLeft;
 
     al_draw_circle(start.x, start.y, DEFAULT_RADIUS, al_map_rgb(0, 255, 0), 1);   
-    al_draw_textf(font, al_map_rgb(0, 0, 0), start.x, start.y - DEFAULT_RADIUS, 
-      ALLEGRO_ALIGN_CENTRE, "%02d", heap[i]);
+    al_draw_textf(font, al_map_rgb(0, 0, 0), start.x, start.y - DEFAULT_RADIUS + 1, 
+      ALLEGRO_ALIGN_CENTRE, "%02d", i);
 
     if (i * 2 <= heap_length)
     {
@@ -153,8 +188,8 @@ void draw_heap(int * heap)
 
     if (soma == i)
     {
-      angle += 30;
-      size -= 30;
+      angle += 5;
+      size *= 0.5;
     }
 
     i++;
@@ -178,25 +213,94 @@ void mouse()
   }
 }
 
+bool is_key_pressed(int key)
+{
+  ALLEGRO_KEYBOARD_STATE s;
+  al_get_keyboard_state(&s);
+  return al_key_down(&s, key);
+}
+
 int main(int argc, char **argv){
 
   int heap[] = {0, 70, 67, 35, 48, 21, 23, 15, 8};
-  heap_length = 8;
+  heap_length = 100;
 
   if (!init())
     return -1;
- 
-  al_set_target_bitmap(buffer);
-  al_clear_to_color(al_map_rgb(255, 255, 255));
-  draw_heap(heap);
 
-  update_screen();
-  al_flip_display();
- 
-  al_rest(15.0);
+  if (heap_length <= 63)
+  {
+    _size = 220;
+    _angle = 5;
+  }
+  else
+  {
+    _size = 440;
+    _angle = 5;
+  }
+  
+  bool left = false;
+  bool right = false;
 
+  while (true)
+  {
+    ALLEGRO_EVENT ev;
+
+    al_wait_for_event(event_queue, &ev);
+
+    if(ev.type == ALLEGRO_EVENT_TIMER) 
+    {
+      redraw = true;
+    }
+    else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) 
+    {
+      break;
+    }
+    else if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
+    {
+      if (ev.keyboard.keycode == ALLEGRO_KEY_LEFT)
+        left = true;
+      else if (ev.keyboard.keycode == ALLEGRO_KEY_RIGHT)
+        right = true;
+      else if (ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+        break;
+    }
+    else if (ev.type == ALLEGRO_EVENT_KEY_UP)
+    {
+      if (ev.keyboard.keycode == ALLEGRO_KEY_LEFT)
+        left = false;
+      else if (ev.keyboard.keycode == ALLEGRO_KEY_RIGHT)
+        right = false;
+    }
+
+    if (left)
+    {
+      camera += Vector2D(10, 0);
+    }
+
+    if (right)
+    {
+      camera += Vector2D(-10, 0);
+    }
+
+    if(redraw && al_event_queue_is_empty(event_queue)) 
+    {
+      redraw = false;
+
+      al_set_target_bitmap(buffer);
+      al_clear_to_color(al_map_rgb(255, 255, 255));
+
+      draw_heap(heap);
+      update_screen();
+    }
+
+    al_flip_display();
+  }
+
+  al_destroy_timer(timer);
   al_destroy_display(screen);
   al_destroy_font(font);
+  al_destroy_event_queue(event_queue);
  
   return 0;
 }
